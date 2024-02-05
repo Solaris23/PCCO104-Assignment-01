e34262d8696a6dae9dc90592d9bfff179eed2c64
Solaris23 <124746015+Solaris23@users.noreply.github.com>
2024-02-05 20:55:47 +0000

    Changed data type int to double and added the missing cases in the switch statement

--- main.cs
+++ main.cs
@@ -1,26 +1,25 @@
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        //Manual input needed
-        int denomination = 100;
+class Program {
+    public static void Main(string[] args) {
+        double denomination = 0.01; // Manual input needed
 
         string personality = getPersonality(denomination);
         Console.WriteLine($"Personality found on the {denomination} bank note: {personality}");
     }
 
-    static string getPersonality(int denomination)
-    {
-        switch (denomination)
-        {
+    static string getPersonality(double denomination) {
+        switch (denomination) {
+            case 0.01:
+            case 0.05:
+            case 0.25:
+                return "No Person is found";
             case 1:
                 return "Jose Rizal";
             case 5:
                 return "Emilio Aguinaldo";
             case 10:
-                return "Andres Bonifacio, Apolinario Mabini";
+                return "Andres Bonifacio & Apolinario Mabini";
             case 20:
                 return "Manuel L. Quezon";
             case 50:
@@ -30,11 +29,11 @@ class Program
             case 200:
                 return "Diosdado Macapagal";
             case 500:
-                return "Benigno Sr. and Corazon Aquino";
+                return "Benigno Sr. & Corazon Aquino";
             case 1000:
-                return "Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda";
+                return "Jose Abad Santos, Vicente Lim & Josefa Llanes Escoda";
             default:
-                return "No Person is found";
+                return $"Invalid Denomination: {denomination}";
         }
     }
 }
